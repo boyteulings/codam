4fd23c70e0f21d86fe9c75385e93db1e75c28f58
Boy Teulings <boyteulings@gmail.com>
2021-10-20 18:19:27 +0200

    ft_memcmp made and makefile norm error fixed

---
+++ core/0/libft/ft_memcmp.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned int		i;
+	const unsigned char	*cs1;
+	const unsigned char	*cs2;
+
+	cs1 = s1;
+	cs2 = s2;
+	i = 0;
+	while ((cs1[i] || cs2[i]) && (n > 0))
+	{
+		if (cs1[i] != cs2[i])
+			return (cs1[i] - cs2[i]);
+		i++;
+		n--;
+	}
+	return (0);
+}
--- core/0/libft/libft.h
+++ core/0/libft/libft.h
@@ -12,5 +12,6 @@ int		ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
