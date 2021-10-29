cad8278a3ce2f0e9ffc30416d51fba5057d22673
Boy Teulings <boyteulings@gmail.com>
2021-10-29 16:09:52 +0200

    ft_calloc finished

--- core/0/libft/libft.h
+++ core/0/libft/libft.h
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
@@ -15,8 +17,9 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 #endif
