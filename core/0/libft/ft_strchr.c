d21a8cf52a4a0d4fee249ad3179fd8f3fe6835a1
Boy Teulings <boyteulings@gmail.com>
2021-11-04 15:42:48 +0100

    ft_strchr and ft_strrchr fixed

--- core/0/libft/ft_strchr.c
+++ core/0/libft/ft_strchr.c
@@ -3,19 +3,13 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 
 	i = 0;
-	if (c == '\0')
-	{
-		while (s[i] != '\0')
-		{
-			i++;
-		}
-		return ((char *)&s[i]);
-	}
 	while (s[i])
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			return ((char *)&s[i]);
 		i++;
 	}
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
 	return (0);
 }
