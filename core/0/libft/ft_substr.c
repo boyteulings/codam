ab901fc18847364c919a0925a72c0cdfd4188650
Boy Teulings <boyteulings@gmail.com>
2021-11-09 19:05:50 +0100

    ft_substr working

---
+++ core/0/libft/ft_substr.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start >= (unsigned int) ft_strlen(s))
+		return (ft_calloc(1, 1));
+	str = (char *) malloc(len + 1);
+	if (str == 0)
+		return (NULL);
+	while (s[i] && i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
--- core/0/libft/libft.h
+++ core/0/libft/libft.h
@@ -31,4 +31,5 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif
