591049a7d66fcfd454ad30d198834aaefbed86f5
Boy Teulings <boyteulings@gmail.com>
2021-11-16 18:21:58 +0100

    started work on ft_split and ft_striteri, continued work on ft_itoa

--- core/0/libft/ft_itoa.c
+++ core/0/libft/ft_itoa.c
@@ -2,14 +2,35 @@
 
 char	ft_itoa(int n)
 {
-	return ();
+	char	*nbr;
+
+	if (n == -2147483648)
+	{
+		n = -2147483648;
+		return (n);
+	}
+	else if (n < 0)
+	{
+		//write(fd, "-", 1);
+		n = -n;
+	}
+	if (n >= 0 && n <= 9)
+	{
+		nbr = n + '0';
+		return (nbr);
+	}
+	else
+	{
+		ft_itoa(n / 10);
+		ft_itoa(n % 10);
+	}
 }
 
 int	main(void)
 {
 	int	n;
 
-	n = 2;
+	n = 22;
 	printf("%c \n", ft_itoa(n));
 	return (0);
 }
---
+++ core/0/libft/ft_split.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+char	**ft_split(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	j;
+	char			str;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+
+	}
+	str[j] = '\0';
+
+	if (str == 0)
+	{
+		free (str);
+		return (NULL);
+	}
+}
