f5a3adea2931a0fdffdd874fade241cf77440db0
Boy Teulings <boyteulings@gmail.com>
2021-11-19 18:25:10 +0100

    worked on ft_itoa, rough sketch done

--- core/0/libft/ft_itoa.c
+++ core/0/libft/ft_itoa.c
@@ -1,36 +1,61 @@
-#include <stdio.h>
+#include <stdio.h> //remove before turn-in
+#include <stdlib.h>
 
-char	ft_itoa(int n)
+/*
+
+char	*ft_itoa(int n)
 {
 	char	*nbr;
+	int		i;
 
+	i = 0;
 	if (n == -2147483648)
 	{
-		n = -2147483648;
-		return (n);
+		nbr = "-2147483648";
+		return (nbr);
 	}
 	else if (n < 0)
 	{
-		//write(fd, "-", 1);
-		n = -n;
+		nbr[0] = '-';
+		i = 1;
 	}
 	if (n >= 0 && n <= 9)
 	{
 		nbr = n + '0';
 		return (nbr);
 	}
-	else
+	else if (n < 0 && n > 9)
 	{
-		ft_itoa(n / 10);
-		ft_itoa(n % 10);
+		(n / 10);
+		(n % 10);
 	}
+}
+
+*/
+
+char *ft_itoa(int n)
+{
+	int		i;
+	int		r;
+	int		v;
+	char	nbr;
+
+	i = 0;
+	r = 0;
+	v = 0;
+	r = n % 10;
+	nbr[i] = r + '0';
+	i--;
+	v = n / 10;
+	nbr[i] = v + '0';
+	i--;
 }
 
 int	main(void)
 {
 	int	n;
 
-	n = 22;
+	n = -2;
 	printf("%c \n", ft_itoa(n));
 	return (0);
 }
