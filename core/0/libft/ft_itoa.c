c5b582a71e613a1584da3fd496bb6835a749803e
Boy Teulings <boyteulings@gmail.com>
2021-11-22 18:54:24 +0100

    ft_itoa no longer segfaults

--- core/0/libft/ft_itoa.c
+++ core/0/libft/ft_itoa.c
@@ -1,5 +1,6 @@
 #include <stdio.h> //remove before turn-in
 #include <stdlib.h>
+#include "libft.h"
 
 /*
 
@@ -33,29 +34,59 @@ char	*ft_itoa(int n)
 
 */
 
-char *ft_itoa(int n)
+int	ft_intlen(int n)
+{
+	if (n >= 1000000000 || n <= -1000000000)
+		return (10);
+	if (n >= 100000000 || n <= -100000000)
+		return (9);
+	if (n >= 10000000 || n <= -10000000)
+		return (8);
+	if (n >= 1000000 || n <= -1000000)
+		return (7);
+	if (n >= 100000 || n <= -100000)
+		return (6);
+	if (n >= 10000 || n <= -10000)
+		return (5);
+	if (n >= 1000 || n <= -1000)
+		return (4);
+	if (n >= 100 || n <= -100)
+		return (3);
+	if (n >= 10 || n <= -10)
+		return (2);
+	return (1);
+}
+
+char	*ft_itoa(int n)
 {
 	int		i;
 	int		r;
 	int		v;
-	char	nbr;
+	char	*nbr;
 
-	i = 0;
+	i = ft_intlen(n) + 1;
 	r = 0;
 	v = 0;
-	r = n % 10;
-	nbr[i] = r + '0';
-	i--;
-	v = n / 10;
-	nbr[i] = v + '0';
-	i--;
+	nbr = (char *) malloc(ft_intlen(n) + 1);
+	while (i >= 0)
+	{
+		r = n % 10;
+		nbr[i] = r + '0';
+		i--;
+		v = n / 10;
+		nbr[i] = v + '0';
+		i--;
+	}
+	nbr[ft_intlen(n)] = '\0';
+	return (nbr);
 }
 
 int	main(void)
 {
 	int	n;
 
-	n = -2;
-	printf("%c \n", ft_itoa(n));
+	n = 1;
+	printf("-------\nOutput: %s \n", ft_itoa(n));
+	printf("ft_intlen: %i \n", ft_intlen(n));
 	return (0);
 }
--- core/0/libft/libft.h
+++ core/0/libft/libft.h
@@ -33,4 +33,5 @@ void	ft_putnbr_fd(int n, int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
 #endif
