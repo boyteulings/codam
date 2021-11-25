b2c3088a732be867b233bb69aef8a688fa3eb3ae
Boy Teulings <boyteulings@gmail.com>
2021-11-25 16:44:22 +0100

    ft_strmapi finished

--- core/0/libft/libft.h
+++ core/0/libft/libft.h
@@ -35,4 +35,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int n);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 #endif
