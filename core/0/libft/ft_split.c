2ee905488190b7238aec13e4244df921fff40964
Boy Teulings <boyteulings@gmail.com>
2021-11-26 17:31:44 +0100

    continued work on ft_split

--- core/0/libft/ft_split.c
+++ core/0/libft/ft_split.c
@@ -1,23 +1,55 @@
 #include <stdlib.h>
+#include <stdio.h> /* REMOVE BEFORE TURNIN */
+#include "libft.h"
+
+/* notes:
+ * splitcount should pass pointer to splits?
+*/
+
+
+unsigned int	func_splitcount(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	splits;
+
+	i = 0;
+	splits = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			if (i == ft_strlen(s))
+			{
+				if (
+				return (splits);
+			}
+			i++;
+			splits++;
+		}
+		i++;
+	}
+	return (splits);
+}
 
 char	**ft_split(char const *s, char c)
 {
 	unsigned int	i;
-	unsigned int	j;
-	char			str;
+	unsigned int	splitcount;
+	unsigned int	chars;
+	char			**str;
 
 	i = 0;
-	j = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
+	splitcount = func_splitcount(s, c);
+	chars = 0;
+	str[splits][chars] = '\0';
+}
 
-	}
-	str[j] = '\0';
+int	main()
+{
+	char	*s;
+	char	c;
 
-	if (str == 0)
-	{
-		free (str);
-		return (NULL);
-	}
+	s = "hoixdoeix";
+	c = 'x';
+	printf("%i \n", splitcount(s, c));
 }
--- core/0/libft/libft.h
+++ core/0/libft/libft.h
@@ -36,4 +36,5 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int n);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	**ft_split(char const *s, char c);
 #endif
