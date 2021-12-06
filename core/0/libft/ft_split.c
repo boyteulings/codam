b4d6249a72510051903e462ccf76eacf93280e31
Boy Teulings <boyteulings@gmail.com>
2021-12-06 18:27:35 +0100

    ft_split almost working with help from kaltevog

--- core/0/libft/ft_split.c
+++ core/0/libft/ft_split.c
@@ -8,7 +8,7 @@
 */
 
 
-unsigned int	fn_splitcount(char const *s, char c)
+unsigned int	fn_wordcount(char const *s, char c)
 {
 	unsigned int	i;
 	unsigned int	splits;
@@ -27,26 +27,98 @@ unsigned int	fn_splitcount(char const *s, char c)
 	return (splits);
 }
 
-char	**ft_split(char const *s, char c)
+/*
+row = 0;
+arr[row] = fn_strreturn;
+row++;
+
+arr[row] = "\0";
+*/
+
+int	fn_wordlen(char const *s, char c, int pos)
 {
-	unsigned int	i;
-	unsigned int	splits;
-	char			*words;
-	char			**str;
+	int	i;
+	int	len;
 
 	i = 0;
-	splits = fn_splitcount(s, c);
-	while (
+	len = 0;
+	while (s[pos] == c && s[pos] != '\0')
+		pos++;
+	while (s[pos] != c && s[pos] != '\0')
+	{
+		pos++;
+		len++;
+	}
+	return (len);
 }
 
+char	*fn_strreturn(char const *s, char c, int pos)
+{
+	int		i;
+	int		dc;
+	int		si;
+	char	*wrd;
 
+	i = 0;
+	dc = 0;
+	si = pos;
+	wrd = (char *)malloc(sizeof(char) * (unsigned long)(fn_wordlen + 1));
+	i = 0;
+	while (s[si] == c)
+		si++;
+	printf("%i, %s \n", si, s);
+	while (s[si])
+	{
+		printf("%c", s[si]);
+		if (s[si] != c)
+		{
+			wrd[i] = s[si];
+			i++;
+		}
+		if (s[si] == c)
+			break ;
+		si++;
+	}
+	wrd[i] = '\0';
+	printf("%s", wrd);
+	return (wrd);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		row;
+	char	*words;
+	char	**array;
+	array = (char **)malloc(sizeof(char *) * (fn_wordcount(s, c) + 1));
+	i = 0;
+	row = 0;
+	array[row] = '\0';
+	return (array);
+}
 
 int	main()
 {
 	char	*s;
 	char	c;
+	char	*check;
 
-	s = "1 2 3";
+	s = "2345         12345678  1234";
 	c = ' ';
-	printf("%i \n", fn_splitcount(s, c));
+	printf("wordcount: %i \n", fn_wordcount(s, c));
+	printf("wordlen: %d \n", fn_wordlen(s, c, 4));
+	check = fn_strreturn(s, c, 6);
+	printf("strreturn: %s \n", check);
 }
+
+//string in positie 1 van dubbel array(row0) row++ en opnieuw
+
+//hey ik wil gesplit worden\0
+//0123456789
+//hey\0
+//ik\0
+//wil\0
+//gesplit\0
+//worden\0
+//\0
+
