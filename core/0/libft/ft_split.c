47f7db04cf922c91d25eeba67dbb08c4312a2305
Boy Teulings <boyteulings@gmail.com>
2021-12-09 17:45:46 +0100

    ft_split finally working and cleaned up, still unprotected

--- core/0/libft/ft_split.c
+++ core/0/libft/ft_split.c
@@ -1,18 +1,10 @@
 #include <stdlib.h>
-#include <stdio.h> /* REMOVE BEFORE TURNIN */
-#include "libft.h" /* REMOVE */
-#include "unistd.h" /* REMOVE */
-
-/* notes:
- * splitcount should pass pointer to splits location?
- * use ft_substr
- */
 
 static int	fn_wordcount(char const *s, char c)
 {
-	unsigned int	i;
-	unsigned int	splits;
-	char			prevchar;
+	int		i;
+	int		splits;
+	char	prevchar;
 
 	prevchar = c;
 	i = 0;
@@ -27,24 +19,14 @@ static int	fn_wordcount(char const *s, char c)
 	return (splits);
 }
 
-/*
-row = 0;
-array[row] = fn_strreturn;
-row++;
-
-array[row] = "\0";
-*/
-
 static int	fn_wordlen(char const *s, char c, int pos)
 {
-	int	i;
 	int	len;
 
-	i = 0;
 	len = 0;
-	while (s[pos] == c && s[pos] != '\0')
+	while (s[pos] == c && s[pos])
 		pos++;
-	while (s[pos] != c && s[pos] != '\0')
+	while (s[pos] != c && s[pos])
 	{
 		pos++;
 		len++;
@@ -70,7 +52,7 @@ static char	*fn_strreturn(char const *s, char c, int *posInString)
 
 	i = 0;
 	si = *posInString;
-	wrd = (char *)malloc(sizeof(char) * (unsigned long)(fn_wordlen + 1));
+	wrd = (char *)malloc(sizeof(char) * (fn_wordlen(s, c, *posInString) + 1));
 	if (!wrd)
 		return (NULL);
 	while (s[si] == c)
@@ -96,13 +78,15 @@ char	**ft_split(char const *s, char c)
 	int		row;
 	char	**array;
 	int		posInString;
+	int		wc;
 
+	wc = fn_wordcount(s, c);
 	posInString = 0;
-	array = (char **)malloc(sizeof(char *) * (fn_wordcount(s, c) + 1));
+	array = (char **)malloc(sizeof(char *) * (wc + 1));
 	if (!array)
 		return (NULL);
 	row = 0;
-	while (row < fn_wordcount(s, c))
+	while (row < wc)
 	{
 		array[row] = fn_strreturn(s, c, &posInString);
 		if (array[row] == NULL)
@@ -112,31 +96,21 @@ char	**ft_split(char const *s, char c)
 	array[row] = NULL;
 	return (array);
 }
+/*
+int	main()
+{
+ 	char	*s;
+ 	char	c;
+ 	char 	**kaas;
 
-// int	main()
-// {
-// 	char	*s;
-// 	char	c;
-// 	char **kaas;
-
-// 	s = "      split       this for   me  !      ";
-// 	c = ' ';
-// 	kaas = ft_split(s, c);
-// 	printf("ft_split output: %s- \n", kaas[0]);
-// 	printf("ft_split output: %s- \n", kaas[1]);
-// 	printf("ft_split output: %s- \n", kaas[2]);
-// 	printf("ft_split output: %s- \n", kaas[3]);
-// 	printf("ft_split output: %s- \n", kaas[4]);
-// 	printf("ft_split output: %s- \n", kaas[5]);
-// }
-
-//string in positie 1 van dubbel array(row0), row++ en dan opnieuw
-
-//hey ik wil gesplit worden\0
-//0123456789
-//hey\0
-//ik\0
-//wil\0
-//gesplit\0
-//worden\0
-//\0
+ 	s = "      split       this for   me  !      ";
+ 	c = ' ';
+ 	kaas = ft_split(s, c);
+ 	printf("ft_split output: %s- \n", kaas[0]);
+ 	printf("ft_split output: %s- \n", kaas[1]);
+ 	printf("ft_split output: %s- \n", kaas[2]);
+ 	printf("ft_split output: %s- \n", kaas[3]);
+ 	printf("ft_split output: %s- \n", kaas[4]);
+ 	printf("ft_split output: %s- \n", kaas[5]);
+}
+*/
