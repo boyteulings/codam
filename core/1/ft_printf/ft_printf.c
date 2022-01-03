ab9ff85ed2fb4e3a5631f68196d3e34b67fc2afe
boyteulings <boyteulings@gmail.com>
2022-01-04 00:53:25 +0100

    little bit of work on ft_printf

--- core/1/ft_printf/ft_printf.c
+++ core/1/ft_printf/ft_printf.c
@@ -7,12 +7,14 @@ int	fn_flag(char *c)
 	int	i;
 
 	i = 0;
-	while (c[i] != '%')
+	while (c[i])
 		i++;
 	if (c[i] == '%')
 	{
-		
+		i++;
+		return (c[i])
 	}
+	else exit ()
 }
 
 int	ft_printf(const char *, )
