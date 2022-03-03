ebfcd1cf549baf7e96d80b20032010cbe592155b
Boy Teulings <boyteulings@gmail.com>
2022-03-03 18:14:27 +0100

    tiny fix in documentation of ft_atoi

--- core/0/libft/ft_atoi.c
+++ core/0/libft/ft_atoi.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// if negative set sign -1 to then do result * -1
+// if negative set sign to -1 then do result * -1
 // result = result * 10 + str[i] - '0';
 // subtract 48 from character to convert ascii to int
 int	ft_atoi(const char *str)
