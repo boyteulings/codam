b36381dde8612cd51a4d0523c230e97779ce8686
Boy Teulings <boyteulings@gmail.com>
2022-08-05 16:38:46 +0200

    fixed missing ; from ft_printf.h

--- core/1/ft_printf/ft_printf.h
+++ core/1/ft_printf/ft_printf.h
@@ -6,12 +6,12 @@
 /*   By: bteuling <bteuling@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/04 16:56:06 by bteuling      #+#    #+#                 */
-/*   Updated: 2022/08/04 16:57:03 by bteuling      ########   odam.nl         */
+/*   Updated: 2022/08/05 16:38:10 by bteuling      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int	ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...);
 #endif
